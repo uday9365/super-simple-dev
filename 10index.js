a1034a01a55638ab4092e4ce6a812e4507637f2b
Udayjit <udayhandique111@gmail.com>
2023-07-28 21:15:13 +0530

    on off button

--- 10index.js
+++ 10index.js
@@ -1,2 +1,42 @@
+// 10c
 const checkBtn = document.querySelector('.js-btn');
 console.log(checkBtn.classList.contains('js-btn')); 
+
+// 10d
+function clickBtn() {
+  const btnElement = document.querySelector('.js-gaming-btn')
+  if (!btnElement.classList.contains('is-toggled')) {
+    btnElement.classList.add('is-toggled')
+  }else{
+    btnElement.classList.remove('is-toggled')
+  }  
+}
+
+// 10e
+
+function clickBtn1() {
+  const btnElement = document.querySelector('.js-gaming1-btn')
+  if (!btnElement.classList.contains('is-toggled')) {
+    btnElement.classList.add('is-toggled')
+  }else{
+    btnElement.classList.remove('is-toggled')
+  }  
+}
+
+function clickBtn2() {
+  const btnElement = document.querySelector('.js-coding-btn')
+  if (!btnElement.classList.contains('is-toggled')) {
+    btnElement.classList.add('is-toggled')
+  }else{
+    btnElement.classList.remove('is-toggled')
+  }  
+}
+
+function clickBtn3() {
+  const btnElement = document.querySelector('.js-traveling-btn')
+  if (!btnElement.classList.contains('is-toggled')){
+    btnElement.classList.add('is-toggled')
+  }else{
+    btnElement.classList.remove('is-toggled')
+  }
+}
