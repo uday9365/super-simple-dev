6240f0ba982a586f7c06fe303022fb925bca2d7c
Udayjit <udayhandique111@gmail.com>
2023-08-05 14:01:30 +0530

    calculator design

---
+++ 10Calculator.js
@@ -0,0 +1,17 @@
+let calculation = localStorage.getItem('calculation') || '';
+
+displayCal();
+
+function updateCalculation (value){
+  calculation += value;
+  displayCal();
+}
+
+function saveCalculation() {
+  localStorage.setItem('calculation' , calculation)
+}
+
+function displayCal() {
+  document.querySelector('.display-result')
+ .innerHTML = calculation;
+}
