4aa359ef82278a626b5aeff7c1ae099aea0a33e0
Udayjit <udayhandique111@gmail.com>
2023-09-03 11:55:33 +0530

    adding date in the toDo list

--- todo-list.js
+++ todo-list.js
@@ -1,4 +1,11 @@
-const todoList  = ["make dinner", "watch Youtube"];
+const todoList  = [{
+  name : "make dinner",
+  dueDate: "02-09-2023"
+ },
+ {
+  name : "watch Youtube",
+  dueDate : "03-09-2023"
+}];
 
 renderTodoList()
 
@@ -6,10 +13,13 @@ function renderTodoList(){
   let todoListHTML = '';
 
   for(let i = 0; i < todoList.length; i++){
-    const todo = todoList[i]
+    const todoObject = todoList[i];
+    //const name = todoObject.name;
+    //const dueDate = todoObject.dueDate;
+    const {name, dueDate} = todoObject
     const html = `
       <p>
-        ${todo}
+        ${name} ${dueDate}
         <button onclick="todoList.splice(${i}, 1);
         renderTodoList();
         ">Delete</button> 
@@ -24,9 +34,16 @@ function renderTodoList(){
 }
 
 function addTodo() {
-  const inputElement = document.querySelector('.js-name-input')
+  const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
-  todoList.push(name)
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    name, 
+    dueDate
+  });
   console.log(todoList)
   inputElement.value = ''; 
   renderTodoList()
